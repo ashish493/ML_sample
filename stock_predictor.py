18b929c043626e5f13487d0ccfcca4e3eebb4dfd
Ashish Malik <44671044+ashish493@users.noreply.github.com>
2019-12-02 09:43:04 +0530

    predicting stock prices
    it uses NSE  tata global data for training .

---
+++ stock_predictor.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+
+#to plot within notebook
+import matplotlib.pyplot as plt
+matplotlib inline
+
+#setting figure size
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 20,10
+
+#for normalizing data
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+#read the file
+df = pd.read_csv('NSE-TATAGLOBAL(1).csv')
+
+#print the head
+df.head()
+
+
+
+# using LSTM
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+
+#creating dataframe
+data = df.sort_index(ascending=True, axis=0)
+new_data = pd.DataFrame(index=range(0,len(df)),columns=['Date', 'Close'])
+for i in range(0,len(data)):
+    new_data['Date'][i] = data['Date'][i]
+    new_data['Close'][i] = data['Close'][i]
+
+#setting index
+new_data.index = new_data.Date
+new_data.drop('Date', axis=1, inplace=True)
+
+#creating train and test sets
+dataset = new_data.values
+
+train = dataset[0:987,:]
+valid = dataset[987:,:]
+
+#converting dataset into x_train and y_train
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+x_train, y_train = [], []
+for i in range(60,len(train)):
+    x_train.append(scaled_data[i-60:i,0])
+    y_train.append(scaled_data[i,0])
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
+
+#predicting 246 values, using past 60 from the train data
+inputs = new_data[len(new_data) - len(valid) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs  = scaler.transform(inputs)
+
+X_test = []
+for i in range(60,inputs.shape[0]):
+    X_test.append(inputs[i-60:i,0])
+X_test = np.array(X_test)
+
+X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+closing_price = model.predict(X_test)
+closing_price = scaler.inverse_transform(closing_price)
+
+train = new_data[:987]
+valid = new_data[987:]
+valid['Predictions'] = closing_price
+plt.plot(train['Close'])
+plt.plot(valid[['Close','Predictions']])
+
+
+#using AUTO Arima
+
+from pyramid.arima import auto_arima
+
+data = df.sort_index(ascending=True, axis=0)
+
+train = data[:987]
+valid = data[987:]
+
+training = train['Close']
+validation = valid['Close']
+
+model = auto_arima(training, start_p=1, start_q=1,max_p=3, max_q=3, m=12,start_P=0, seasonal=True,d=1, D=1, trace=True,error_action='ignore',suppress_warnings=True)
+model.fit(training)
+
+forecast = model.predict(n_periods=248)
+forecast = pd.DataFrame(forecast,index = valid.index,columns=['Prediction'])
+
+
+plt.plot(train['Close'])
+plt.plot(valid['Close'])
+plt.plot(forecast['Prediction'])
+
+
+#using KNN
+
+#importing libraries
+from sklearn import neighbors
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+#scaling data
+x_train_scaled = scaler.fit_transform(x_train)
+x_train = pd.DataFrame(x_train_scaled)
+x_valid_scaled = scaler.fit_transform(x_valid)
+x_valid = pd.DataFrame(x_valid_scaled)
+
+#using gridsearch to find the best parameter
+params = {'n_neighbors':[2,3,4,5,6,7,8,9]}
+knn = neighbors.KNeighborsRegressor()
+model = GridSearchCV(knn, params, cv=5)
+
+#fit the model and make predictions
+model.fit(x_train,y_train)
+preds = model.predict(x_valid)
+#plot
+valid['Predictions'] = 0
+valid['Predictions'] = preds
+plt.plot(valid[['Close', 'Predictions']])
+plt.plot(train['Close'])
